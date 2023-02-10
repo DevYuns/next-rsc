b23b2d7001fea1e505581e18ba65c3a3c81c9f89
DevYuns <ydhun93@gmail.com>
2023-02-10 12:21:27 +0900

    fix: remove unused files

--- lib/utils/getTranslation.ts
+++ lib/utils/getTranslation.ts
@@ -3,7 +3,7 @@ import 'server-only';
 import type {Locale} from '../i18n';
 
 // We enumerate all dictionaries here for better linting and typescript support
-// We also get the defalt import for cleaner types
+// We also get the default import for cleaner types
 const translates = {
   en: () => import('./translates/en.json').then((module) => module.default),
   ko: () => import('./translates/ko.json').then((module) => module.default),
@@ -15,5 +15,6 @@ export type Translation = {
   [K in keyof Translates]: Translates[K];
 };
 
-export const getTranslates = async (locale: Locale): Promise<Translates> =>
-  translates[locale]();
+export const getTranslates = async (locale: Locale): Promise<Translates> => {
+  return translates[locale]();
+};
